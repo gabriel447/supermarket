5e3c668b3b922f075ba355d45b8242ac918847e9
Gabriel447 <fullstackgabe.com>
2023-12-02 22:38:12 -0300

    properties, entity, ORM, seed

---
+++ src/main/java/dev/gabriel447/supermarket/entities/Product.java
@@ -0,0 +1,67 @@
+package dev.gabriel447.supermarket.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    Double price;
+    Integer quantity;
+
+    public Product(Long id, String name, Double price, Integer quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product product)) return false;
+        return Objects.equals(getId(), product.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_product (name, price, quantity) VALUES ('macbook', 6.000, 10);
+UPDATE tb_product SET price = 6000 WHERE id = 1;
