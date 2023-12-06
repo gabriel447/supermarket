9860c449b0fec3ac784bcd82a0565910e05debfb
Gabriel <fullstackgabe@gmail.com>
2023-12-06 09:07:55 -0300

    Constructor Methods

--- src/main/java/dev/gabriel447/supermarket/entities/Product.java
+++ src/main/java/dev/gabriel447/supermarket/entities/Product.java
@@ -16,6 +16,15 @@ public class Product {
     private Double price;
     private Integer quantity;
 
+    public Product() {}
+
+    public Product(Long id, String name, Double price, Integer quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
     public Long getId() { return id; }
 
     public void setId(Long id) {
