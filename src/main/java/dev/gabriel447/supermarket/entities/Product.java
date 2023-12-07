7725301a0e847c962d07ec45b943ac2f85acae9d
Gabriel <fullstackgabe@gmail.com>
2023-12-07 17:37:03 -0300

    super

--- src/main/java/dev/gabriel447/supermarket/entities/Product.java
+++ src/main/java/dev/gabriel447/supermarket/entities/Product.java
@@ -19,6 +19,7 @@ public class Product {
     public Product() {}
 
     public Product(Long id, String name, Double price, Integer quantity) {
+        super();
         this.id = id;
         this.name = name;
         this.price = price;
